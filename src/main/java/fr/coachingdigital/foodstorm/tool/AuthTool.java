3bd4a333c8e3e1c991ca63b481c4cbab8cfd6309
Jean-Philippe <jphetr@gmail.com>
2018-04-24 16:56:32 +0200

    SONAR FIX

--- src/main/java/fr/coachingdigital/foodstorm/tool/AuthTool.java
+++ src/main/java/fr/coachingdigital/foodstorm/tool/AuthTool.java
@@ -14,61 +14,75 @@ import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Tool for authentification
- * 
+ *
  * @author jetrilla
  *
  */
 public class AuthTool {
 
 	/**
-	 * Method that generate RSAPublicKey from filr
-	 * @param publicKey the filr representing public key
-	 * @return the rsa public key
-	 */
-	public static RSAPublicKey generatePublicKey(File publicKey) {
-		RSAPublicKey pubKey = null;
-		try {
-			DataInputStream dis = new DataInputStream(new FileInputStream(publicKey));
-			byte[] pubKeyBytes = new byte[(int) publicKey.length()];
-			dis.readFully(pubKeyBytes);
-			dis.close();
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(pubKeyBytes);
-			pubKey = (RSAPublicKey) keyFactory.generatePublic(pubSpec);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-		}
-		return pubKey;
-	}
-
-	/**
 	 * Method that generate RSAPrivateKey from file
 	 * @param privateKey the file representing private key
 	 * @return the rsa private key
 	 */
-	public static RSAPrivateKey generatePrivateKey(File privateKey) {
+	public static RSAPrivateKey generatePrivateKey(final File privateKey) {
 		RSAPrivateKey privKey = null;
+		DataInputStream dis = null;
 		try {
-			DataInputStream dis = new DataInputStream(new FileInputStream(privateKey));
-			byte[] privKeyBytes = new byte[(int) privateKey.length()];
+			dis = new DataInputStream(new FileInputStream(privateKey));
+			final byte[] privKeyBytes = new byte[(int) privateKey.length()];
 			dis.read(privKeyBytes);
-			dis.close();
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privKeyBytes);
+			final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			final PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privKeyBytes);
 			privKey = (RSAPrivateKey) keyFactory.generatePrivate(privSpec);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
+		} catch (final NoSuchAlgorithmException e) {
 			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
+		} catch (final InvalidKeySpecException e) {
 			e.printStackTrace();
+		}finally {
+			if(dis != null) {
+				try {
+					dis.close();
+				} catch (final IOException e) {
+				}
+			}
 		}
 		return privKey;
+	}
+
+	/**
+	 * Method that generate RSAPublicKey from filr
+	 * @param publicKey the filr representing public key
+	 * @return the rsa public key
+	 */
+	public static RSAPublicKey generatePublicKey(final File publicKey) {
+		RSAPublicKey pubKey = null;
+		DataInputStream dis = null;
+		try {
+			dis = new DataInputStream(new FileInputStream(publicKey));
+			final byte[] pubKeyBytes = new byte[(int) publicKey.length()];
+			dis.readFully(pubKeyBytes);
+			final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			final X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(pubKeyBytes);
+			pubKey = (RSAPublicKey) keyFactory.generatePublic(pubSpec);
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		} catch (final NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (final InvalidKeySpecException e) {
+			e.printStackTrace();
+		}finally {
+			if(dis != null) {
+				try {
+					dis.close();
+				} catch (final IOException e) {
+				}
+			}
+		}
+		return pubKey;
 	}
 
 }
