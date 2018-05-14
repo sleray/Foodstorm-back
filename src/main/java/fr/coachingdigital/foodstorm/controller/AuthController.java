260c20392c2ce2d94060544736648814ce76bb25
Jean-Philippe <jphetr@gmail.com>
2018-05-14 16:45:10 +0200

    DELETE crossconfig

--- src/main/java/fr/coachingdigital/foodstorm/controller/AuthController.java
+++ src/main/java/fr/coachingdigital/foodstorm/controller/AuthController.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +14,6 @@ import fr.coachingdigital.foodstorm.model.Member;
 import fr.coachingdigital.foodstorm.service.AuthService;
 import fr.coachingdigital.foodstorm.service.MemberService;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
